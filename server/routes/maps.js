53e1b3ed3d51fe3add768c1ad96aae17cc7a8e92
gbarrbrown <gbb7899@gmail.com>
2019-03-19 13:46:40 +1300

    can return all maps from db

--- server/routes/maps.js
+++ server/routes/maps.js
@@ -1,15 +1,17 @@
 const express = require('express')
 const router = express.Router()
+const { getNewMaps } = require('../db/maps')
 
 // GET /api/v1/maps/new-round
 router.get('/new-round', (req, res) => {
-    res.json([
-        {name: "Paris",
-        lat_long: [51.505, -0.09],
-        zoom: 7
-    },
-    "Welly", "Taupo", "Bangkok"
-    ])
+    getNewMaps()
+    .then((maps) => {
+        res.json(maps)
+    })
+    .catch((err) => {
+        console.log(err)
+        res.status(500).json({error: 'Something went wrong'})
+    }) 
 })
 
 // POST /api/v1/maps/results/:results
